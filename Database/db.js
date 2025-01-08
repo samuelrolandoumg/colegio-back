01644cb61b5555097af25fffc9cae380a015e0ca
samuelrolandumg <smartinezl2@miumg.edu.g>
2025-01-07 19:01:20 -0600

    comit 8

--- Database/db.js
+++ Database/db.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   { 
-    host: process.env.DB_SERVER,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306, // Puerto (Railway lo define)
+    DB_HOST: process.env.DB_HOST,
+    DB_DIALECT: process.env.DB_DIALECT,
+    DB_PORT: process.env.DB_PORT || 3306, // Puerto (Railway lo define)
     logging: false,
     dialectOptions: process.env.DB_DIALECT === 'mysql' ? {
       options: {
