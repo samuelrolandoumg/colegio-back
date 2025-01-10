d4b9aff633153a1698a851f575ae0f4b6f8c54ff
samuelrolandumg <smartinezl2@miumg.edu.g>
2025-01-10 01:41:34 -0600

    first commit122218211223

--- Database/db.js
+++ Database/db.js
@@ -1,21 +1,46 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   {
+//     host: dbConfig.host,
+//     port: dbConfig.port,
+//     dialect: "mysql", // Dialecto
+//     dialectOptions: {
+//       ssl: {
+//         require: true, // Opcional: Si Railway requiere SSL
+//         rejectUnauthorized: false, // Ignora certificados no autorizados
+//       },
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+
+const dbConfig = require("./db.config.js");
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
   dbConfig.database,
   dbConfig.username,
   dbConfig.password,
-  {
-    host: dbConfig.host,
+  {    host: dbConfig.host,
     port: dbConfig.port,
-    dialect: "mysql", // Dialecto
-    dialectOptions: {
-      ssl: {
-        require: true, // Opcional: Si Railway requiere SSL
-        rejectUnauthorized: false, // Ignora certificados no autorizados
-      },
-    },
+    dialect: dbConfig.dialect,
+    operatorsAliases: 0,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
   }
-);
-
+);const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Usuarios = require("../models/Usuarios.js")(sequelize, Sequelize);
 module.exports = sequelize;
-
--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,38 +1,68 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../Database/db.js');
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../Database/db.js');
 
-const Usuarios = sequelize.define('Usuarios', {
-    id_usuario: {
-        type: DataTypes.STRING(50),
-        primaryKey: true,
-    },
-    nombre: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-    },
-    correo: {
-        type: DataTypes.STRING(100),
-//        unique: true,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-    },
-    rol: {
-        type: DataTypes.STRING(50), // Cambiado de ENUM a STRING
-        allowNull: false,
-        validate: {
-            isIn: [['Alumno', 'Profesor', 'Admin']], // Validación a nivel de Sequelize
-        }, 
-    },
-    estado: {
-        type: DataTypes.BOOLEAN,
-    },
-}, {
-    tableName: 'Usuarios',
-    timestamps: false,
-});
+// const Usuarios = sequelize.define('Usuarios', {
+//     id_usuario: {
+//         type: DataTypes.STRING(50),
+//         primaryKey: true,
+//     },
+//     nombre: {
+//         type: DataTypes.STRING(100),
+//         allowNull: false,
+//     },
+//     correo: {
+//         type: DataTypes.STRING(100),
+// //        unique: true,
+//         allowNull: false,
+//     },
+//     password: {
+//         type: DataTypes.STRING(255),
+//         allowNull: false,
+//     },
+//     rol: {
+//         type: DataTypes.STRING(50), // Cambiado de ENUM a STRING
+//         allowNull: false,
+//         validate: {
+//             isIn: [['Alumno', 'Profesor', 'Admin']], // Validación a nivel de Sequelize
+//         }, 
+//     },
+//     estado: {
+//         type: DataTypes.BOOLEAN,
+//     },
+// }, {
+//     tableName: 'Usuarios',
+//     timestamps: false,
+// });
 
-module.exports = Usuarios;
+// module.exports = Usuarios;
 
+
+
+module.exports = (sequelize, Sequelize) => {
+        const Usuarios = sequelize.define('Usuarios', {
+            id_usuario: { type: Sequelize.STRING(50),
+                primaryKey: true,
+            },
+            nombre: {type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            correo: {type: Sequelize.STRING(100),
+                //        unique: true,
+                        allowNull: false,
+                    },
+                    password: {type: Sequelize.STRING(255),
+                        allowNull: false,
+                    },
+                    rol: {type: Sequelize.STRING(50), // Cambiado de ENUM a STRING
+                        allowNull: false,
+                        validate: {
+                            isIn: [['Alumno', 'Profesor', 'Admin']], // Validación a nivel de Sequelize
+                        }, 
+                    },
+                    estado: {type: Sequelize.BOOLEAN,
+                    },
+                }, {
+                    tableName: 'Usuarios',timestamps: false,
+                });
+                
+                module.exports = Usuarios;};
